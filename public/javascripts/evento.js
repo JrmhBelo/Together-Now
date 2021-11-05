f3eda8209e7ff5bec8f59499cbaa56d7af127d3b
J0A0-A <>
2021-11-05 22:39:59 +0000

    mais imformaçoes nos eventos
    descomentar evento hmtl + js

--- public/javascripts/evento.js
+++ public/javascripts/evento.js
@@ -11,13 +11,13 @@ window.onload = async function() {
 
         document.getElementById("nome").innerHTML = evento.eve_nome;
         document.getElementById("descricao").innerHTML = evento.eve_descricao;
-        /*document.getElementById("data").innerHTML = evento.eve_data;
-        document.getElementById("horaI").innerHTML = evento.eve_datai;
-        document.getElementById("horaF").innerHTML = evento.eve_dataf;
+        // document.getElementById("data").innerHTML = evento.eve_data;
+        // document.getElementById("horaI").innerHTML = evento.eve_datai;
+        // document.getElementById("horaF").innerHTML = evento.eve_dataf;
         document.getElementById("maxPart").innerHTML = evento.eve_maxparticipantes;
         document.getElementById("beneficiario").innerHTML = evento.eve_beneficiario;
         document.getElementById("estado").innerHTML = evento.eve_estado;
-        document.getElementById("categoria").innerHTML = evento.eve_categ;*/
+        document.getElementById("categoria").innerHTML = evento.eve_categ;
             
         
     } catch(err) {
