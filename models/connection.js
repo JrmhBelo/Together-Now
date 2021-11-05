293f6580b79aa146e9f783ed7d9b8d00f66df47e
JrmhBelo <jrmhbelo@gmail.com>
2021-11-05 15:07:05 +0000

    fix

--- models/connection.js
+++ models/connection.js
@@ -4,14 +4,11 @@ const connectionString = "postgres://postgres:banana@localhost:5432/togethernow"
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
-    max: 10
-})
-/*
-,
+    max: 10,
     ssl: {
         require: true, 
         rejectUnauthorized: false
     }
-*/
+})
 
 module.exports = pool;
