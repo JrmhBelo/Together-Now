0ba3cad3c05d71ff7f4a8a30e84c409901128ad8
tomasbourdain <tomasbourdain@gmail.com>
2021-11-05 23:14:15 +0000

    update

--- models/eventoModel.js
+++ models/eventoModel.js
@@ -41,8 +41,8 @@ module.exports.getEventoById = async function(id) {
 
 module.exports.saveEvento = async function(evento) { 
     try { 
-    let sql ="Insert Into evento (eve_nome, eve_beneficiario, eve_descricao, eve_data, eve_datai, eve_dataf, eve_categ, eve_maxparticipantes, eve_lati, eve_lon) Values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)";
-    let result = await pool.query( sql , [evento.nomeId, evento.beneficiarioId, evento.descricaoId, evento.dataId, evento.dataIID, evento.dataFId, evento.categoriaId, evento.maxPartId, evento.eve_lati, evento.eve_lon]); 
+    let sql ="Insert Into evento (eve_nome, eve_beneficiario, eve_descricao, eve_datai, eve_dataf, eve_dataf, eve_horai, eve_horaf eve_categ, eve_maxparticipantes, eve_lati, eve_lon) Values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)";
+    let result = await pool.query( sql , [evento.nomeId, evento.beneficiarioId, evento.descricaoId, evento.dataIId, evento.dataFID, evento.horaIId, evento.horaFId, evento.categoriaId, evento.maxPartId, evento.eve_lati, evento.eve_lon]); 
     return { status:200, result: result }; } catch (err) { 
                console.log(err);
                return { status:500, result: err};
