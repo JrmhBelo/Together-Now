3862ca0dd51f21a0cb3ca6164fe70fd8d3012df9
J0A0-A <73562671+J0A0-A@users.noreply.github.com>
2021-11-06 13:29:17 +0000

    Delete Insert.sql

--- Documentação/PostgreSQL/Insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.evento(
-	 eve_data, eve_datai, eve_dataf, eve_morada, eve_maxparticipantes, eve_lati, eve_long, eve_nome, eve_descricao, eve_beneficiario, eve_estado, eve_categ)
-	VALUES ( '2001-10-05', '04:00:00', '04:20:00', 'Rua da vida', 20, 20.888, 80.558, 'Super evento', 'evento solidario', 'banco alimentar', 'Iniciar', 'interior');
