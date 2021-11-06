555a766546815a68d6ccc433a54c879b1f166210
tomasbourdain <tomasbourdain@gmail.com>
2021-11-06 17:01:41 +0000

    update

--- public/javascripts/formularioEvento.js
+++ public/javascripts/formularioEvento.js
@@ -69,8 +69,8 @@ async function criar() {
             horaFId: document.getElementById("horaF").value,
             categoriaId: document.getElementById("categoria").value,
             maxPartId: document.getElementById("maxPart").value,
-            latId: document.getElementById("latitude").textContent, 
-            lonId: document.getElementById("longitude").textContent,
+            latId: document.getElementById("latitude").value,
+            lonId: document.getElementById("longitude").value,
             //moradaId: document.getElementById("morada").value,
             //gesId = sessionStorage.getItem("gesId");
         };
