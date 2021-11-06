111c2a7920a6a423023a550448e69c89d0d89852
tomasbourdain <tomasbourdain@gmail.com>
2021-11-06 01:45:46 +0000

    update

--- models/eventoModel.js
+++ models/eventoModel.js
@@ -60,7 +60,7 @@ module.exports.changeEstado = async function(evento) { try {
 
 module.exports.saveRegisto = async function(registo) { 
     try { 
-    let sql ="Insert Into regista (reg_morada, reg_escivil, reg_escol, reg_empreg, reg_prof, reg_motiv, uti_id, eve_id) Values ($1,$2,$3,$4,$5,$6,$7,$8)";
+    let sql ="Insert Into regista (reg_morada, reg_civil, reg_escol, reg_empreg, reg_prof, reg_motiv, uti_id, eve_id) Values ($1,$2,$3,$4,$5,$6,$7,$8)";
     let result = await pool.query( sql , [registo.moradaId, evento.escivilId, evento.escolaridadeId, evento.sitEmpregoId, evento.profissaoId, registo.motivacaoId, registo.utiId, registo.eventoId]); 
     return { status:200, result: result }; } catch (err) { 
                 console.log(err);
