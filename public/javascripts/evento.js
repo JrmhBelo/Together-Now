78166b1051627f333c95bb20b3f12e835d2c5ba7
tomasbourdain <73538684+tomasbourdain@users.noreply.github.com>
2021-11-09 15:12:40 +0000

    update

--- public/javascripts/evento.js
+++ public/javascripts/evento.js
@@ -11,8 +11,8 @@ window.onload = async function() {
 
         document.getElementById("nome").innerHTML = evento.eve_nome;
         document.getElementById("descricao").innerHTML = evento.eve_descricao;
-        document.getElementById("dataI").innerHTML = evento.eve_datai;
-        document.getElementById("dataF").innerHTML = evento.eve_dataf;
+        document.getElementById("dataI").innerHTML = toDateString(evento.eve_datai);
+        document.getElementById("dataF").innerHTML = toDateString(evento.eve_dataf);
         document.getElementById("horaI").innerHTML = evento.eve_horai;
         document.getElementById("horaF").innerHTML = evento.eve_horaf;
         document.getElementById("maxPart").innerHTML = evento.eve_maxparticipantes;
