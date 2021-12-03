fec0d4ab97a7ef1d060b13c84ac698c1760005e9
J0A0-A <>
2021-12-03 15:00:33 +0000

    Update evento.js

--- public/javascripts/evento.js
+++ public/javascripts/evento.js
@@ -13,10 +13,10 @@ window.onload = async function() {
 
         document.getElementById("nome").innerHTML = evento.eve_nome;
         document.getElementById("descricao").innerHTML = evento.eve_descricao;
-        document.getElementById("dataI").innerHTML = evento.eve_datai;
-        document.getElementById("dataF").innerHTML = evento.eve_dataf;
-        document.getElementById("horaI").innerHTML = evento.eve_horai;
-        document.getElementById("horaF").innerHTML = evento.eve_horaf;
+        //document.getElementById("dataI").innerHTML = evento.eve_datai;
+        //document.getElementById("dataF").innerHTML = evento.eve_dataf;
+        //document.getElementById("horaI").innerHTML = evento.eve_horai;
+        //document.getElementById("horaF").innerHTML = evento.eve_horaf;
         document.getElementById("maxPart").innerHTML = evento.eve_maxparticipantes;
         document.getElementById("beneficiario").innerHTML = evento.eve_beneficiario;
         document.getElementById("estado").innerHTML = evento.eve_estado;
