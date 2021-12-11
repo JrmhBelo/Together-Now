01eb2480f443f9cc774c6228671e2b7769c2ff98
tomasbourdain <tomasbourdain@gmail.com>
2021-12-11 14:10:48 +0000

    update mapa

--- public/javascripts/mapa.js
+++ public/javascripts/mapa.js
@@ -1,9 +1,15 @@
 var mymap;
-var eventos = [];
-var eventos;
+var Interiormarker = new Array();
+var Exteriormarker = new Array();
+var greenIcon;
+var redIcon;
+var latPosition;
+var lonPosition;
 
 window.onload = async function () {
-    mymap = L.map('mapid').setView([38.707325418964764, -9.152454160542419], 10);
+    getEventos();
+    getCurrentPosition();
+    mymap = L.map('mapid').setView([38.707325418964764, -9.152454160542419], 11);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,54 +20,176 @@ window.onload = async function () {
         accessToken: 'pk.eyJ1IjoibWJ1Z2FsaG8iLCJhIjoiY2phOWdhbWR5MGprdDJ5cDgzenR5MXMxMCJ9.n38CZPOHiDjdbKXw2YuEmA'
     }).addTo(mymap);
 
+    L.Control.geocoder().addTo(mymap);
+
+    greenIcon = L.icon({
+        iconUrl: '../images/placeholder.png',
+        iconSize:     [50, 50],
+        iconAnchor:   [25, 54], 
+        popupAnchor:  [13, -50] 
+    });
+
+    redIcon = L.icon({
+        iconUrl: '../images/location.png',
+        iconSize:     [70, 70], 
+        iconAnchor:   [30, 54], 
+        popupAnchor:  [25, -30] 
+    });
+
     eventos = await $.ajax({
         url:"/api/eventos",
         method: "get",
         dataType: "json"
     });
 
-    for (let evento of eventos)
-        
-        if (evento.eve_lati) {
-            var marker = L.marker([evento.eve_lati, evento.eve_lon]).addTo(mymap);
+    for (let evento of eventos){
+        if (evento.eve_lati && evento.eve_categ=='Interior') {
+            var marker = L.marker([evento.eve_lati, evento.eve_lon],{icon: greenIcon}).addTo(mymap).bindPopup(`<h4 onclick='showEvento(${evento.eve_id})'><u>${evento.eve_nome}</u>
+                                                                                                            <br>${evento.eve_descricao}<br> Data de Inicio: ${formatDate(evento.eve_datai)}
+                                                                                                            <br> Data de Fim: ${formatDate(evento.eve_dataf)}<br> Categoria: ${evento.eve_categ}</h4>`);
+            Interiormarker.push(marker);
             eventos.push({evento: evento, marker: marker});
-            evento.marker = marker;   
+        }else if(evento.eve_categ=='Exterior') {
+            var mymarker = L.marker([evento.eve_lati, evento.eve_lon],{icon: redIcon}).addTo(mymap).bindPopup(`<h4 onclick='showEvento(${evento.eve_id})'><u>${evento.eve_nome}</u>
+                                                                                                            <br>${evento.eve_descricao}<br> Data de Inicio: ${formatDate(evento.eve_datai)}
+                                                                                                            <br> Data de Fim: ${formatDate(evento.eve_dataf)}<br> Categoria: ${evento.eve_categ}</h4>`);
+            ~Exteriormarker.push(mymarker);
+            eventos.push({evento: evento, marker: mymarker});
         }
     }
-    
-/* 38.7077, -9.1364
-//preciso de fazer um add das coordenadas aqui tipo Array [[10.5,-3.56]]
-coords = []
-//preciso fazer um add das datas de inicio 
-dInicio = []
-//preciso fazer um add das datas de fim
-dFim = []
-//preciso de fazer um add de imagens dos eventos
-imagens= []
-//preciso de fazer um add de todos os eventos ex: evento1, evento2
-eventos =[]
+    };
 
-let l = coords.length;
 
-var event = document.querySelector('#evento');
-
-for ( let i = 0; i < l; i++) {
-      //popups
-      var pop = L.popups({
-        closeonClick: true
-    }).setContent('<h4>Data de Inicio: ' + dInicio[i] + '<br> Data de Fim: ' + dFim[i] + '</h4><img src=' + imagens[i] + ' style="height: 100px;">');
-    //markers
-    var marker = L.marker(coords[i]).addTo(mymap).bindPopup(pop);
-    //labels
-    var toollip = L.tooltip({
-        permanent:true
-    }).setContent(dInicio[i])
-
-    marker.bindTooltip(toollip);
-
-    // zoom in /fly to
-    eventos[i].addEventListener("mouseover", ()=> {
-        mymap.flyto(coords[i], 16)
-    })
+async function getEventos(){
+    let elem = document.getElementById("eventos");
+    try {
+        let eventos = await $.ajax({
+        url: "/api/eventos",
+        method: "get",
+        dataType: "json"
+        });
+        let html ="";
+        for(let evento of eventos)
+            html += `<section onmouseover="getEvento(${evento.eve_id})" onclick="getRota(${evento.eve_id})"'>
+            ${evento.eve_nome}</section>`
+        elem.innerHTML = html;
+    } catch(err) {
+        console.log(err);
+        elem.innerHTML = "<h1> Page not Available </h1>";
+    }
 }
-*/
+
+async function getEvento(id){
+    sessionStorage.setItem("eventoId",id);
+    evento = await $.ajax({
+        url:"/api/eventos/"+id,
+        method: "get",
+        dataType: "json"
+    });
+    lat = evento.eve_lati;
+    lon = evento.eve_lon
+    mymap.flyTo([lat, lon], 12)
+
+}
+
+async function getRota(id){
+    sessionStorage.setItem("eventoId",id);
+    evento = await $.ajax({
+        url:"/api/eventos/"+id,
+        method: "get",
+        dataType: "json"
+    });
+
+    routeControl = L.Routing.control({
+        waypoints: [
+          L.latLng(latPosition, lonPosition),
+          L.latLng(evento.eve_lati,evento.eve_lon)
+        ],
+        routeWhileDragging: true,
+        geocoder: L.Control.Geocoder.nominatim(),
+      }).addTo(mymap);
+
+}
+
+function createEventosHTML(eventos) {
+    let html = "";
+    for(let evento of eventos){
+            html += `<section onmouseover="getEvento(${evento.eve_id})" onclick="getRota(${evento.eve_id})"'>
+            ${evento.eve_nome}</section>`
+    }
+    document.getElementById("eventos").innerHTML = html;
+}
+    
+async function filtrar() {
+    try {
+        let categoria= document.getElementById("categoria").value;   
+        
+        
+        let eventos = await $.ajax({
+            url: `/api/eventos/search/?categoria=${categoria}`,
+            method: "get",
+            dataType: "json"
+        
+        });
+        createEventosHTML(eventos);
+        filtrarMarkers(categoria);
+    } catch(err) {
+        
+        console.log(err);
+        }
+}
+
+async function filtrarMarkers(categoria) {
+    if (categoria == 'Interior') {
+        for(i = 0; i< Exteriormarker.length; i++){
+            mymap.removeLayer(Exteriormarker[i]);
+        }
+        if (Interiormarker){     
+            for(m = 0; m< Interiormarker.length; m++){
+                Interiormarker[m].addTo(mymap)
+            }
+        }
+
+    }else if(categoria == 'Exterior') {
+        for(i = 0; i< Interiormarker.length; i++){
+            mymap.removeLayer(Interiormarker[i]);
+        }
+        if (Exteriormarker){    
+            for(m = 0; m< Exteriormarker.length; m++){
+                Exteriormarker[m].addTo(mymap)
+            }
+        }
+        }
+    
+}
+
+async function getCurrentPosition() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }
+}
+
+function showPosition(position) {
+    lonPosition = position.coords.longitude
+    latPosition = position.coords.latitude
+}
+
+function showEvento(id) {
+    sessionStorage.setItem("eventoId",id);
+    window.location = "evento.html";
+}
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [day, month, year].join('-');
+}
+ 
