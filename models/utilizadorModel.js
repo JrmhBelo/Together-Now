514e6edb488e8b49948cc9bbcbb2ad298735f01e
J0A0-A <73562671+J0A0-A@users.noreply.github.com>
2022-02-08 22:07:53 +0000

    mais css

--- models/utilizadorModel.js
+++ models/utilizadorModel.js
@@ -31,7 +31,7 @@ module.exports.loginUtilizador = async function(email,pass) { // esta feito ☻
         let result = await pool.query(sql,[email,pass]);
         if (result.rows.length > 0)
             return { status:200, result:result.rows[0]};
-        else return { status:401, result: {msg: "Wrong email or password"}};
+        else return { status:401, result: {msg: "▬▬ Wrong email or password"}};
     } catch (err) {
         console.log(err);
         return { status:500, result: err};
